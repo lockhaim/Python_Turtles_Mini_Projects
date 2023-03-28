a917dd490b021479ee24aa91a3b18a88caf5634e
Ian Lockhart <lockhaim@gmail.com>
2023-03-28 15:28:09 -0600

    Added psuedocode and objectives in comments

---
+++ Balloon_Popper.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+## OBJECTIVES ##
+#detect inputs
+#require multiple inputs to pop balloon
+#utilize variable conditions and functions
+
+## PSEUDOCODE ##
+## Draw Balloon
+## Up Key Pressed
+## Is the balloon at max size?
+##  --If No
+##  ----Increase balloon size
+##  ----then go to draw step
+##  --If Yes
+##  ----Clear Balloon
+##  ----Display "POP!"
+
