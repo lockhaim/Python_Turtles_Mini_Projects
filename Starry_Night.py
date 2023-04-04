46782edc0fd4306c0af5cf64d39a4f95335e4feb
Ian Lockhart <lockhaim@gmail.com>
2023-04-04 16:17:48 -0600

    error handling using round function

--- Starry_Night.py
+++ Starry_Night.py
@@ -31,8 +31,8 @@ def draw_star(xpos, ypos): #star generation
 
 
 for x in range(125):     # (0,0) is the center of the screen so all stars currently in NE quadrant if the range doesnt start at negative
-    xpos = randrange(-width/2, width/2) #uses a full screen size in window if not /2
-    ypos = randrange(-height/2, height/2) #dividing width and height by 2 will give us all dots in the default window size
-    draw_star(xpos, ypos)
+    xpos = randrange(round(-width/2), round(width/2)) #uses a full screen size in window if not /2
+    ypos = randrange(round(-height/2), round(height/2)) #dividing width and height by 2 will give us all dots in the default window size
+    draw_star(xpos, ypos) #used the 'round' function so that when the width or height calculation gave a floating point decimal the final number would be a whole integer instead
 
 done()
