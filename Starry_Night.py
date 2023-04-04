cbdb2811cfbb6860b00b1370e6c7c332aef6c96e
Ian Lockhart <lockhaim@gmail.com>
2023-04-04 15:40:54 -0600

    created a draw star function with star coordinates as parameters

--- Starry_Night.py
+++ Starry_Night.py
@@ -10,5 +10,14 @@ from turtle import *
 # END
 
 bgcolor("black")
+hideturtle() #hide the turtle so as not to obstruct our 'stars'
+
+def draw_star(xpos, ypos): #star generation
+    penup()
+    goto(xpos, ypos)
+    pendown()
+    dot(20, "white")
+
+draw_star(0, 0) #test star
 
 done()
